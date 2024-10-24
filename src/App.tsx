ff4a4f8b6f71b4f4e18438a816d36d60ae7953ea
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-24 12:52:10 +0530

    feat:tailwindcss added

--- src/App.tsx
+++ src/App.tsx
@@ -14,42 +14,42 @@ import { clusterApiUrl } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { SolBalance } from "./SolBalance";
-import { SolAirDrop } from "./SolAirdrop";
+import { SolBalance } from "./componets/SolBalance";
+import { SolAirDrop } from "./componets/SolAirdrop";
 
 function App() {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
- 
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[]}>
-        <WalletModalProvider>
-          <WalletMultiButton />
-          <WalletDisconnectButton />
-          <Connect/>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <>
+      <div className="text-3xl font-bold underline text-center">
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={[]}>
+            <WalletModalProvider>
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+              <Connect />
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </div>
+    </>
   );
 }
 
-function Connect(){
-  const {connected} = useWallet()
+function Connect() {
+  const { connected } = useWallet();
   return (
     <>
-      {connected&&(
+      {connected && (
         <>
-        <SolBalance/>
-        <SolAirDrop/>
+          <SolBalance />
+          <SolAirDrop />
         </>
       )}
     </>
-  )
+  );
 }
 
-
-
-
-export default App
+export default App;
---
+++ src/componets/SolAirdrop.tsx
@@ -0,0 +1,32 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useCallback } from "react";
+
+export function SolAirDrop() {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
+  const onClick = useCallback(async () => {
+    try {
+      if (!publicKey) throw new Error("wallet not conncted");
+      var signature = await connection.requestAirdrop(
+        publicKey,
+        LAMPORTS_PER_SOL
+      );
+    } catch (error: any) {
+      console.log(error);
+    }
+  }, [connection, publicKey]);
+
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        disabled={!publicKey}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Request AirDrop
+      </button>
+    </div>
+  );
+}
